30e709dc007381f53493b5ed24cbd35727a8c309
wintimes <842110408@qq.com>
2020-04-15 13:29:26 +0800

    kaikeba

---
+++ kaikeba/ex1-1.js
@@ -0,0 +1,16 @@
+//获取元素
+document.getElementById('btn');
+console.log(document.getElementById('btn'));
+/*
+    document - file
+    . - 的
+    getElementById
+        - get
+        - Element
+        - By
+        - Id
+    在当前文档中通过id获取元素
+
+    JS 是区分大小写的！！
+
+ */
