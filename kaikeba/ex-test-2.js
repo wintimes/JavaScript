78731914b40ecbb8efb3fb74b59dfe9c0997af59
wintimes <842110408@qq.com>
2020-04-15 16:50:57 +0800

    chapter1

--- kaikeba/ex-test-2.js
+++
@@ -1,9 +0,0 @@
- //获取元素
-document.getElementById('btn');
-console.log(document.getElementById('btn'))
-//第二种方法 
-document.querySelector('#btn1');
-console.log(document.querySelector('#btn1'));
-document.querySelector('.box');
-console.log(document.querySelector('.wrap box'));
-// console.log(document.querySelector('.wrap box'));
