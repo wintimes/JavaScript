31ac2a3ecc42c738f605dfc6320d83ff67fd7d6b
wintimes <842110408@qq.com>
2020-04-16 06:09:47 +0800

    chapter1

--- chapter1/basic.js
+++ chapter1/basic.js
@@ -44,3 +44,52 @@ var x2 = "He is called \"Tom\"";
 var one = "Hello,jack.";
 var two = "i'm rose";
 result = one + two;
+var myNum = 123;
+var myString = myNum.toString();
+typeof myString;
+//字符串转换
+var myString = "123";
+var myNum = Number(myString);
+typeof myNum;
+//查询字符串长度
+var myString = "hello world";
+myString.length;
+//查询第一个字符；
+myString[0];
+///在字符串中查询子字符串并且提取
+//str.indexOf(searchValue, fromIndex);
+"Blue Sky".indexOf("Blue"); // returns  0
+"Blue Sky".indexOf("Ble"); // returns -1
+"Blue Sky".indexOf("Sky", 0); // returns  5
+"Blue Sky".indexOf("Sky", -1); // returns  5
+"Blue Sky".indexOf("Sky", 5); // returns  5
+"Blue Sky".indexOf("Sky", 9); // returns -1
+"Blue Sky".indexOf("", 0); // returns  0
+"Blue Sky".indexOf("", 5); // returns 5
+"Blue Sky".indexOf("", 9); // returns 8
+
+"Blue Sky".indexOf("blue");//returns -1
+//slice(strat，end)，第一个参数 start 是开始提取的字符位置，
+// 第二个参数 end 是提取的最后一个字符的后一个位置。
+// 所以提取从第一个位置开始，直到但不包括最后一个位置。
+// 另外第二个参数也可以不写，不写代表某个字符之后提取字符串中的所有剩余字符。
+// 比如：
+"Blue sky".slice(0, 3);
+// 注：slice(strat，end)，第一个参数 start 是开始提取的字符位置，
+// 第二个参数 end 是提取的最后一个字符的后一个位置。所以提取从第一个位置开始，
+// 直到但不包括最后一个位置。
+// 另外第二个参数也可以不写，不写代表某个字符之后提取字符串中的所有剩余字符。比如：
+"Blue sky".slice(2, 4);//returns "ue"
+var string ="I like study";
+string.toLowerCase(); // returns "i like study"
+string.toUpperCase(); // returns "I LIKE STUDY"
+
+var string = "I like study";
+string.replace("study", "sleep"); // returns "I like sleep"
+var num = 8;
+var numString = num.toString();
+numString;//returns "8"
+var result = true;
+var resultString = result.toString();
+resultString;//returns "true"
+var num = 16;
