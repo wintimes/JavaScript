cf83e0f94a702ab298e9cd934f77f7671a90605d
wintimes <842110408@qq.com>
2020-04-15 13:41:02 +0800

    kaikeba

--- kaikeba/ex1-1.js
+++ kaikeba/ex1-1.js
@@ -12,5 +12,9 @@ console.log(document.getElementById('btn'))
     在当前文档中通过id获取元素
 
     JS 是区分大小写的！！
-
+    console.log(放置想检查的元素)
  */
+//第二种方式,通过元素选择器来选择
+document.querySelector('#btn')
+document.querySelector('.box')
+console.log(document.querySelector('#btn'))
