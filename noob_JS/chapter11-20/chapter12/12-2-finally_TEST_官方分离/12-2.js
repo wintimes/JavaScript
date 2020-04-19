7b26a08b7a316d9ca6b632cc465c909ca1fce7a4
wintimes <842110408@qq.com>
2020-04-19 13:10:10 +0800

    chapter12

---
+++ noob_JS/chapter11-20/chapter12/12-2-finally_TEST_官方分离/12-2.js
@@ -0,0 +1,19 @@
+function myFunction() {
+  var message, x;
+  message = document.getElementById("p01");
+  message.innerHTML = "";
+  x = document.getElementById("demo").value;
+  try {
+    if(x == "") throw "值是空的";
+    if(isNaN(x)) throw "值不是一个数字";
+    x = Number(x);
+    if(x > 10) throw "太大";
+    if(x < 5) throw "太小";
+  }
+  catch(err) {
+    message.innerHTML = "错误: " + err + ".";
+  }
+  finally {
+    document.getElementById("demo").value = "";
+  }
+}
